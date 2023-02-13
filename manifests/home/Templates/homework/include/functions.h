86eb0517468bcd16d8dc1d65df0d34cea1568b1d
davidliyutong <davidliyutong@sjtu.edu.cn>
2023-02-13 22:00:06 +0800

    feat: add track of configuration to manifests/home

---
+++ manifests/home/Templates/homework/include/functions.h
@@ -0,0 +1,5 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H // The macro is used to avoid multiple include 
+
+void function(); // function declaration
+#endif
---
+++ manifests/home/Templates/homework/src/functions.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "functions.h"
+
+void function() {
+    // Function implementation.
+    // Do something...
+    printf("Function call\n");
+}
