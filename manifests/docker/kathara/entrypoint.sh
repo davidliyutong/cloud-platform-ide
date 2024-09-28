11e7be447c42039ae404559f87992cf4b6e730c5
davidliyutong <davidliyutong@sjtu.edu.cn>
2024-09-28 22:25:11 +0800

    Fix dockerd pid Update example docker compose
    fix: kathara flavor have dedicated entrypoint to clean /var/run/docker.pid
    
    fix: fix broken, outdated docker compose configuration

---
+++ manifests/docker/kathara/entrypoint.sh
@@ -0,0 +1,12 @@
+DEPLOY_HOME=/home/ubuntu
+trap "echo exitting" 15
+if [ ! -f "$DEPLOY_HOME/.config/code-server/CONFIGURED" ]; then
+    echo "Init config with default"
+    echo "Password: none"
+    sudo tar -xf /opt/home.tar.xz -C $DEPLOY_HOME --no-same-owner
+    sudo rsync -a /root/ $DEPLOY_HOME/
+fi
+
+echo "---------------------------"
+sudo rm -rf /var/run/docker.pid
+sudo -E /startup.sh
